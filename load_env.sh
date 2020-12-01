cffc1057f03a95a54cf04e3239d0eea5278d6709
y-du <42994541+y-du@users.noreply.github.com>
2020-12-01 12:29:44 +0100

    add host ip as env var

--- load_env.sh
+++ load_env.sh
@@ -38,6 +38,7 @@ conf_vars=(
 env_vars=(
     "MGW_CORE_PATH=$core_dir"
     "MGW_CORE_DIR_NAME=${core_dir##*/}"
+    "MGW_HOST_IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)"
 )
 
 
